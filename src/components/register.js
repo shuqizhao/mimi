e9d92af5834fa69a9623556924a9c87ffbce89ad
shuqizhao <shuqizhao@qq.com>
2018-01-13 16:17:11 +0800

    layout

--- src/components/register.js
+++ src/components/register.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-const List = r => require.ensure([], () => r(require('../components/list.vue')), 'list')
 
+const Home = r => require.ensure([], () => r(require('../components/home.vue')), 'home')
+Vue.component('home', Home);
+
+const Login = r => require.ensure([], () => r(require('../components/login.vue')), 'login')
+Vue.component('login', Login);
+
+
+const List = r => require.ensure([], () => r(require('../components/list.vue')), 'list')
 Vue.component('list', List);
