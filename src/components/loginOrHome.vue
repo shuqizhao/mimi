4aba47badc3ed8e1d2aa761fe364a84e1c3f3a3f
shuqizhao <shuqizhao@qq.com>
2018-01-18 19:38:48 +0800

    watch cookie

--- src/components/loginOrHome.vue
+++ src/components/loginOrHome.vue
@@ -4,20 +4,34 @@
 </template>
 
 <script>
-
 const Home = r =>
   require.ensure([], () => r(require("../components/home.vue")), "home");
 
 const Login = r =>
   require.ensure([], () => r(require("../components/login.vue")), "login");
 export default {
-//   props: ["isLogin"],
+  //   props: ["isLogin"],
   data() {
     return {
-        isLogin:this.getCookie(this.getGlobalData().LoginCookeName)
+      isLogin: this.getCookie(this.getGlobalData().LoginCookeName),
+      isNeedWatch:false
     };
   },
   mounted: function() {
+    this.watchLoginCookie();
+    if(this.isLogin){
+      this.isNeedWatch = true;
+    }
+  },
+  methods: {
+    watchLoginCookie: function() {
+      var self = this;
+      setInterval(function() {
+        if (!self.getCookie(self.getGlobalData().LoginCookeName)&&self.isNeedWatch) {
+            window.location.reload()
+        }
+      }, 100);
+    }
   },
   components: {
     home: Home,
